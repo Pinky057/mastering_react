be3b8ca67f09cef4e1e85690e48ec4a480f092e3
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-20 01:36:59 +0600

    components button list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, {Component} from'react';
 class Counter extends Component{
     state = {
         count:0,
-
+ tags:['tag1', 'tag2', 'tag3']
     };
 
     render() {
@@ -13,6 +13,10 @@ class Counter extends Component{
 
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tags.map(tag =><li key={tag}>{ tag }</li>)}
+        </ul>
+
         </div>
     );
     }

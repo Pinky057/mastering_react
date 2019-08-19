ad5eb870bea1aef041cfb3ca18c95c470ba5d12a
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-19 17:11:03 +0600

    arrow function job

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const jobs =[
+{id: 1, isActive: true},
+{id: 2, isActive: true},
+{id: 3, isActive: false},
+];
+const activeJobs = jobs.filter(function (job) {
+    return job.isActive;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}); // usual function
+const activeJobs = jobs.filter(job=>job.isActive); //arrow function

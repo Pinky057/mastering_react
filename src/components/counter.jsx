fb04280512d93ed7e10da06462c2f8e8212fd24e
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-20 01:24:12 +0600

    components button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,19 +5,24 @@ class Counter extends Component{
         count:0,
 
     };
-    styles = {
-        fontSize:50,
-        fontWeight: "bold"
-    };
+
     render() {
+
         return (
         <div>
 
-            <span style={this.styles} className="badge badge-danger m-2">{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
     );
     }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount(){
         const { count } = this.state;
         return count ===0? "Zero" : count;

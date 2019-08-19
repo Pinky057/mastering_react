f5e08469ba5c672d537d86bc5f4c3895e747e992
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-19 16:54:05 +0600

    arrow function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const sqre=function(nmbr){
+    return nmbr*nmbr;
+}
+console.log(sqre(7));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const sqr = nmbr=>nmbr*nmbr;//arrow funtion
+console.log(sqr(5));

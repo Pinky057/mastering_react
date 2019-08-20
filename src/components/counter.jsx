b39fb42e7dda33c1f8c71652faf11a10fc9b60fb
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-21 01:29:57 +0600

    event handlers adding delete button state property accessing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -28,8 +28,10 @@ doHandelIncrement=()=>{
                 onClick={this.doHandelIncrement}
                 className="btn btn-secondary btn-sm">Increment
             </button>
-            <button onClick={this.props.onDelete}
-                className="btn btn-danger btn-sm m-2">Delete</button>
+            <button
+                onClick ={()=>{this.props.onDelete(this.props.id)}}
+                className="btn btn-danger btn-sm m-2">Delete
+            </button>
 
         </div>
     );

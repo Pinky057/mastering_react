e2f9143ae5386501a1d75479658714fe27b24368
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-20 22:29:58 +0600

    multiple component passing data to components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, {Component} from'react';
 
 class Counter extends Component{
     state = {
-        count:this.props.value,
+        value:this.props.value, //changing to count to value for cleaner code and understanding code.
  tags:['tag1', 'tag2', 'tag3']
     };
 
@@ -14,7 +14,7 @@ class Counter extends Component{
 
 handelIncrement =(product)=>{
     console.log(product);
-    this.setState({count: this.state.count + 1});
+    this.setState({value: this.state.value + 1});
 };
 
 doHandelIncrement=()=>{
@@ -37,13 +37,13 @@ doHandelIncrement=()=>{
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return count ===0? "Zero" : count;
+        const { value} = this.state;
+        return value ===0? "Zero" : value;
     }
 }
 export default Counter;

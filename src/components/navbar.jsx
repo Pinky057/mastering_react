3f3325b1f2f5e29997365c5c4e2f1e9a51aca358
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-21 14:47:31 +0600

    lifting state up and making controlled components adding counter numbers in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,10 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar
+                <a className="navbar-brand" href="#">
+                    Navbar <span className="badge badge-pill badge-secondary">
+                    {this.props.totalCounters}
+                </span>
                 </a>
             </nav>
         );

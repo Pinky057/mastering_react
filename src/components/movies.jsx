5003bd51cf7c1528dbcdf23fff72a51243b0cfbf
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-22 02:15:21 +0600

    like included updated movie app

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,66 +3,72 @@ import {getMovies} from "../services/fakeMovieService";
 import Like from "./common/like";
 
 class Movies extends Component {
-    state={
-        movies:getMovies()
+    state = {
+        movies: getMovies()
     };
 
-    handleLike = movie =>{
-        const movies ={...this.state.movies};
-        const index =movies.indexOf(movie);
-        movies[index]={...movies[index]};
-        movies[index].liked=!movies[index].liked;
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        debugger;
+        movies[index] = {...movies[index],liked:!movies[index].liked};
+        //console.log(movies[index]);
+        //movies[index].liked = !movies[index].liked;
         this.setState({movies});
     };
 
 
-
-    handleDelete= movie =>{
-        const movies=this.state.movies.filter(m=> m._id !==movie._id);
+    handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
         this.setState({movies});
     };
+
     render() {
-        const {length: count }= this.state.movies;
-        if(count ===0)
+        const {length: count} = this.state.movies;
+        if (count === 0)
             return <p> There are no movies in the database.</p>;
 
         return (
             <React.Fragment>
-            <p>Showing {count} movies in the database.</p>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Stock</th>
-                    <th>Rate</th>
-                    <th/>
-                    <th/>
-                </tr>
-                </thead>
-                <tbody>
-                {this.state.movies.map(movie => (<tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                        <Like liked={movie.liked}
-                              onClick={()=>{this.handleLike(movie)}}/>
-                    </td>
-                    <td>
-                        <button onClick={()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
-                    </td>
-                </tr>))}
+                <p>Showing {count} movies in the database.</p>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                        <th/>
+                        <th/>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.movies.map(movie => (<tr key={movie._id}>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                        <td>
+                            <Like liked={movie.liked}
+                                  onClick={() => {
+                                      this.handleLike(movie)
+                                  }}/>
+                        </td>
+                        <td>
+                            <button onClick={() => this.handleDelete(movie)}
+                                    className="btn btn-danger btn-sm">Delete
+                            </button>
+                        </td>
+                    </tr>))}
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
             </React.Fragment>
 
         );
     }
 
 
-    }
+}
 
 export default Movies;
--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -1,6 +1,6 @@
 import * as genresAPI from "./fakeGenreService";
 
-const movies = [
+const movies = [  
   {
     _id: "5b21ca3eeb7f6fbccd471815",
     title: "Terminator",

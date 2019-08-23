85551c04feca98f455655060499e177e01e0e7a4
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-24 00:55:23 +0600

    extracting tableHeader

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
 
+
+
+
+
+
+//interface
+//coloums:array
+//sortColoumn:obj
+//onSort:func
+
 class TableHeader extends Component {
+
+
+    raiseSort =path =>{
+        const sortColumn ={...this.props.sortColumn};
+        if (sortColumn.path=== path)
+            sortColumn.order = sortColumn.order==="asc"? "desc":"asc";
+        else{
+            sortColumn.path =path;
+            sortColumn.order='asc';
+        }
+        this.props.onSort(sortColumn);
+    };
+
+
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <thead>
+            <tr>
+                {this.props.columns.map(column=>
+                    <th key={column.path||column.key} onClick={()=>this.raiseSort(column.path)}>
+                        {column.label }</th>)}
+            </tr>
+            </thead>
         );
     }
 }

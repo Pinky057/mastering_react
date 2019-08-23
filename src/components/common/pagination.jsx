f36561500047fb3939208f2d48e30730f98f7547
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-24 00:28:50 +0600

    moving responsibility

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -16,7 +16,7 @@ const Pagination = props  =>{
             {pages.map(page=>(
                 <li key={page}
                     className={page === currentPage ?'page-item active':'page-item'}>
-                    <a href="" className="page-link"
+                    <a href="#" className="page-link"
                        onClick={()=>onPageChange(page)}>{page}
                     </a>
                 </li>

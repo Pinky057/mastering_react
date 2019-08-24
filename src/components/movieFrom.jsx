a91a254e13175ddc36117bd8e2fde54151f9ef8d
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-25 01:37:34 +0600

    movie  from

--- src/components/movieFrom.jsx
+++ src/components/movieFrom.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const MovieFrom =({match}) => {
-    return <h1>Movie From{match.params.id}</h1>;
+const MovieFrom =({match, history}) => {
+    return(
 
-
+      <div>
+    <h1>Movie From{match.params.id}</h1>
+          <button className="btn btn-primary"
+                  onClick={()=>history.push("/movies")}>
+              Save
+          </button>
+      </div>
+    ) ;
 }
 
 export default MovieFrom;

77512ef511feb9527882ccc97c92dfe340fd7ecf
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-24 22:20:05 +0600

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Route} from'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Redirect, Switch} from'react-router-dom';
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
@@ -9,11 +9,14 @@ import './App.css';
 function App() {
   return (
 <main className="container">
+  <Switch>
   <Route path="/movies" component={Movies}></Route>
   <Route path="/customers" component={Customers}></Route>
   <Route path="/rentals" component={Rentals}></Route>
   <Route path="/notFound" component={NotFound}></Route>
-
+  <Redirect from="/" exact to="/movies" />
+  <Redirect to="/not-found"/>
+  </Switch>
 </main>
   );
 }

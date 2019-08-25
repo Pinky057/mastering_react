bb1b98a1b93e5d3a499b26fa28750a8a2e2323f6
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-25 18:59:07 +0600

    extracting input login from

--- src/components/loginFrom.jsx
+++ src/components/loginFrom.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Input from "./common/input";
+
 
 class LoginFrom extends Component {
     state ={
@@ -30,27 +32,20 @@ class LoginFrom extends Component {
             <div>
                 <h1>Login</h1>
                 <form  onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            value={account.username}
-                            onChange={this.handleChange}
-                            autoFocus
-                            name="username"
-                            id="username"
-                            type="text"
-                            className="form-control"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            value={account.password}
-                            onChange={this.handleChange}
-                            name="password"
-                            id= "password"
-                            type="text"
-                            className="form-control"/>
-                    </div>
+                 <Input
+                     name="username"
+                     value={account.username}
+                     label="Username"
+                     onChange={this.handleChange}/>
+
+                    <Input
+                        name="password"
+                        value={account.password}
+                        label="Password"
+                        onChange={this.handleChange}/>
+
+
+
                     <button className="btn btn-primary">Login</button>
                 </form>
 

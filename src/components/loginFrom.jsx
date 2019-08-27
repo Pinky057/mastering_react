2b2e3fcb5c692d09749380120553bd0fe7cc5e66
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-27 23:25:53 +0600

    validating from with Joi

--- src/components/loginFrom.jsx
+++ src/components/loginFrom.jsx
@@ -112,7 +112,8 @@ return errors;
 
 
 
-                    <button className="btn btn-primary">Login</button>
+                    <button   disabled={this.validate()}
+                        className="btn btn-primary">Login</button>
                 </form>
 
             </div>

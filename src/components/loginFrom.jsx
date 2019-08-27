2859bf0fec1cb84f8f23b40146c1eb0ef935fbbd
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-28 00:15:24 +0600

    validating from with Joi

--- src/components/loginFrom.jsx
+++ src/components/loginFrom.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Input from "./common/input";
 import Joi from 'joi-browser';
+import Form from './common/form';
 
 
-class LoginFrom extends Component {
+class LoginFrom extends Form {
     state ={
-        account:{username:'', password:''},
+        data:{username:'', password:''},
         errors:{}
     };
 
@@ -21,82 +22,23 @@ class LoginFrom extends Component {
        this.username.current.focus();
    }*/
 
-  validate=()=>{
-      const options={abortEarly:false};
-      const {error}=Joi.validate(this.state.account, this.schema,options);
 
-
-      if (!error) return null;
-      const errors={};
-      for (let item of error.details)
-          errors[item.path[0]]= item.message;
-return errors;
-
-/*
-      const errors={};
-      const {account}=this.state;
-      if(account.username.trim()==='')
-          errors.username= "Username is required";
-      if(account.password.trim()==='')
-          errors.password="Password is Required";
-
-      return Object.keys(errors).length === 0? null : errors;*/
-  };
-
-    handleSubmit= e=>{
-        e.preventDefault();
-
-       const errors =this.validate();
-       this.setState({errors: errors|| {}});
-       if (errors) return;
-
-
+    doSubmit=()=>{
+        //call the server
         console.log("Submitted");
     };
-    validateProperty =({name, value})=>{
-        const obj={[name]:value};
 
-        const schema={[name]: this.schema[name]};
-        const{error}=Joi.validate(obj, schema);
-        return error? error.details[0].message: null;
-
-        /*if(error) return  null;
-        return error.details[0].message;*/
-
-   /* if (name==='username')   {
-        if (value.trim()==='')
-            return "Username is required";
-    }
-
-        if (name==='password')   {
-            if (value.trim()==='')
-                return "Password is required";
-        }*/
-
-    };
-
-    handleChange= ({currentTarget: input})=>{
-        const errors={...this.state.errors};
-        const errorMessage=this.validateProperty(input);
-        if (errorMessage) errors[input.name]=errorMessage;
-        else delete errors[input.name];
-
-        const account={...this.state.account};
-        account[input.name]=input.value;
-        this.setState({account, errors});
-
-    };
 
 
     render() {
-        const{account, errors}=this.state;
+        const{data, errors}=this.state;
         return (
             <div>
                 <h1>Login</h1>
                 <form  onSubmit={this.handleSubmit}>
                  <Input
                      name="username"
-                     value={account.username}
+                     value={data.username}
                      label="Username"
                      onChange={this.handleChange}
                      error={errors.username}
@@ -104,7 +46,7 @@ return errors;
 
                     <Input
                         name="password"
-                        value={account.password}
+                        value={data.password}
                         label="Password"
                         onChange={this.handleChange}
                         error={errors.password}

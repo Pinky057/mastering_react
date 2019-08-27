6eb83d3a3b756115072928332f41409f5f7dd810
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-08-27 19:13:15 +0600

    validating from with Joi

--- src/components/loginFrom.jsx
+++ src/components/loginFrom.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Input from "./common/input";
+import Joi from 'joi-browser';
 
 
 class LoginFrom extends Component {
@@ -8,12 +9,22 @@ class LoginFrom extends Component {
         errors:{}
     };
 
-    username= React.createRef();
+    schema ={
+      username:Joi.string().required(),
+      password:Joi.string().required()
+    };
+
+
+
+   // username= React.createRef();
   /* componentDidMount() {
        this.username.current.focus();
    }*/
 
   validate=()=>{
+      const result=Joi.validate(this.state.account, this.schema,{abortEarly:false});
+      console.log(result);
+
       const errors={};
       const {account}=this.state;
       if(account.username.trim()==='')

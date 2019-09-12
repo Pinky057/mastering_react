da54f42facb1ebecb600b05e3c50475832c6efa3
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-13 03:16:45 +0600

    weather app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            positionError => console.log(positionError)
+        );
+
+        return <div>hi there !!!</div>
+    }
+}
+
+
+ReactDom.render(
+    <App/>,
+    document.querySelector('#root')
+);

4d2c9abd5446213f545515b1fd862b41916b1ada
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-18 22:36:07 +0600

    weather app

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,23 +1,34 @@
 import React from 'react';
 
+const seasonConfig={
+  summer:{
+      text:"Let's hit the Beach!!",
+      iconName:'sun'
 
+  },
+  winter:{
+      text:"It's Chilly!!",
+      iconName:'snowflake'
+  }
+};
 
 const getSeason=(lat, month)=>{
     if (month >2 && month<9){
-       return lat > 0 ?'Summer' : 'Winter';
+       return lat > 0 ?'summer' : 'winter';
     }
     else{
-        return lat > 0 ?'Winter' : 'Summer';
+        return lat > 0 ?'winter' : 'summer';
     }
 };
 
 const SeasonDisplay=(props)=> {
     const season= getSeason(props.lat, new Date().getMonth());
-    const text = season ==='Winter'?'Burr its Chilly' :'lets hit the beach';
-
+    const {text, iconName}=seasonConfig[season];
     return (
         <div>
+            <i className={`${iconName} icon`}/>
             <h1>{text}</h1>
+            <i className={`${iconName} icon`}/>
         </div>
     );
 }

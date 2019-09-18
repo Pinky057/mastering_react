439d0f02ba2048c2b80455390a24b55e610be2de
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-18 21:10:41 +0600

    weather app

--- src/index.js
+++ src/index.js
@@ -6,20 +6,25 @@ class App extends Component {
 
     constructor(props){
         super(props);
-        this.state={lat:null};
+        this.state={lat:null, errorMessage:''};
 
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat:position.coords.latitude});
-            },
-            positionError => console.log(positionError)
-        );
     }
+componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({lat:position.coords.latitude}),
+        positionError => this.setState({errorMessage:positionError.message})
+    );
+}
+
 
     render() {
-
-
-        return <div>Latitude:{this.state.lat}</div>
+          if(this.state.errorMessage && !this.state.lat){
+              return <div>Error:{this.state.errorMessage}</div>;
+          }
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude:{this.state.lat}</div>;
+        }
+        return <div>Loading</div>;
     }
 }
 

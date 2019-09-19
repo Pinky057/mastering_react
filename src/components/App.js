3f172b8d6df687f574922d6a93bbdc448681a521
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-19 22:55:28 +0600

    Image search App

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,17 @@ import SearchBar from "./SearchBar";
 
 
 class App extends Component {
-    onSearchSubmit(term){
-       axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term){
+       const response= await axios
+           .get('https://api.unsplash.com/search/photos', {
            params:{query: term},
            headers:{
                Authorization:
                    'Client-ID 1ffe2d23f8aa9097b8d985086170637bea838eebc6de5181363221ba5920ee89'
            }
 
-       }).then((response)=>{      //working with promise
-           console.log(response.data.results);
-       })
+       });
+        console.log(response.data.results);
     }
 
     render() {

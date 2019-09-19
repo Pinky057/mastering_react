dbaae52fa49879eb440631f19bfe580400edd232
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-20 05:58:30 +0600

    Image search App

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const ImageList=(props)=> {
-    console.log(props.images);
+  const images= props.images.map(({description,id, urls})=>{
+   return <img alt={description} key={id} src={urls.regular}/>
+  });
     return (
         <div>
-   imageList
+   {images}
         </div>
     );
 }

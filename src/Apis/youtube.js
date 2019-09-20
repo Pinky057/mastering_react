82e7b39ac9b0ec24b9544228d472baf5455a6fb1
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-21 02:04:17 +0600

    youtube video search app

---
+++ src/Apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY='AIzaSyDa6ocAHg7a8ueRToTRDFlVZ0qdQAFrM4U';
+
+export default axios.create({
+        baseURL:'GET https://www.googleapis.com/youtube/v3',
+    params:{
+            part:'snippet',
+        maxResults:5,
+        key:KEY
+    }
+    });

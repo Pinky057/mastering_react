a3736079e57903b9b17436d4b56095aad8cdd319
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-20 23:40:49 +0600

    Image search App

---
+++ src/components/searchBar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>video search</label>
+                        <input type="text"/>
+                    </div>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default SearchBar;

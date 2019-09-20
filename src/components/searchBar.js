14f4aed5b21e6b40a12f7c52026cdc53d5a58fc3
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-09-21 00:38:30 +0600

    video search app

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,13 +2,26 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
     state={term:''};
+
+    onInputChane=(event)=>{
+        this.setState({term: event.target.value});
+
+    };
+    onFormSubmit=(event)=>{
+        event.preventDefault();
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>video search</label>
-                        <input type="text"/>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChane}
+                        />
                     </div>
                 </form>
 

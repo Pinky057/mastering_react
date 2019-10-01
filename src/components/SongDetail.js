dfdb457920f22521e423d503c6e98fc399e94d0a
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-10-02 00:22:38 +0600

    redux with songs list

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {connect} from'react-redux';
 
-const SongDetail=(props)=>{
-    console.log(props);
+const SongDetail=({song})=>{
+  if (!song){
+      return <div>Select a Song</div>
+  }
     return (
         <div>
-   songDetail
+            <h3>Details for:</h3>
+            <p>
+            Title:{song.title}
+            <br/>
+            Duration:{song.duration}
+            </p>
         </div>
     );
 };

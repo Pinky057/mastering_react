12a40f3ac64f89aefbefc3b4f228d31f5efdd3f0
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-10-20 00:27:04 +0600

    redux with post blog

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class QuizBee extends Component{
+    state={
+        questionBank: [],
+        score:0,
+        response:0
+};
+    computeAnswer=(answer, correctAnswer)=>{
+     if (answer===correctAnswer){
+         this.setState({
+             score: this.state.score + 1
+         });
+     }
+     this.setState({
+         response: this.state.response< 5? this.state.response+1:5
+     })
+    };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    getQuestions=()=>{
+      quizService().then(question=>{
+         this.setState({
+             questionBank:question
+         }) ;
+      });
+    };
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <div className="title">QuizBee</div>
+                {this.state.questionBank.length >0 &&
+                this.state.response<5 &&
+                this.state.questionBank.map(
+                    ({question,answers, correct,questionId})=>(
+                        <QuestionBox
+                            question={question}
+                            options={answers}
+                            key={questionId}
+                         selected={answer=>this.computeAnswer(answer, correct)}
+                        />
+                    )
+                )}
+            </div>
+        );
+    }
+
+}
+ReactDOM.render(<QuizBee/>, document.getElementById("root"));

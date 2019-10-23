fdcf0a2cdde535592e3b4657b84cd23b48ce94da
ishratjahanpinky2@gmail.com <ishratjahanpinky2@gmail.com>
2019-10-23 20:51:07 +0600

    recipe search with edmam api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import Recipe from "./Recipe";
 
-function App() {
+const App=()=> {
+  const APP_ID ="b2b58825";
+  const APP_KEY="ebf26e3ba916ec070e0726d80e082c23";
+  //const exampleReq=`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [recipes, setRecipes]=useState([]);
+
+  useEffect(()=>{
+ getRecipes();
+  },[]);
+
+  const getRecipes=async ()=>{
+    const response= await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+   const data=await response.json();
+   setRecipes(data.hits);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <form className="search-bar">
+          <input className="search-bar" type="text"/>
+          <button className="search-button" type="submit">Search</button>
+        </form>
+        {recipes.map(recipe=>(
+            <Recipe
+                key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            />
+        ))}
+      </div>
   );
 }
 
